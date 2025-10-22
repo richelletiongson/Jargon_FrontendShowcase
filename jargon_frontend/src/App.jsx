29865f58b01d55e387434c20df9b62159bd91064
richelletiongson <richelletiongson@icloud.com>
2025-10-22 13:08:26 -0700

    Set up pages and folders

--- jargon_frontend/src/App.jsx
+++ jargon_frontend/src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from "./components/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Course from "./pages/Course";
+import AI from "./pages/AI";
+import Profile from "./pages/Profile";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+            <div className='page'>
+                <main className='main-content'>
+                    <Routes>
+                        <Route
+                            path='/'
+                            element={<Home />}
+                        />
+                        <Route
+                            path='/browse'
+                            element={<Course />}
+                        />
+                        <Route
+                            path='/my-movies'
+                            element={<AI />}
+                        />
+                        <Route
+                            path='/watchlist'
+                            element={<Charts />}
+                        />
+                        <Route
+                            path='/favourites-myreviews'
+                            element={<Profile />}
+                        />
+                        
+                    </Routes>
+                </main>
+                <NavBar />
+            </div>
+        </BrowserRouter>
   )
 }
 
